b6c64791069f6c6ec21dde784013b8b3fded15d4
sw15_sunkyu <azon731@gmail.com>
2021-03-26 22:59:19 +0900

    210326

---
+++ Baekjoon/1463.py
@@ -0,0 +1,14 @@
+
+dp = [-1 for _ in range(1000001)]
+dp[1] = 0
+x = int(input())
+
+for i in range(2, x+1):
+    dp[i] = dp[i - 1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    dp[i] = min(dp[i], dp[i - 1] + 1)
+
+print(dp[x])
